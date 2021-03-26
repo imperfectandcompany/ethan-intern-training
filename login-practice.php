4a4959fe6bc26e0f64b14ad1a4234d430df0a5c2
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-03-26 13:20:57 -0700

    Boiler Plate
    Initial Layout With Boiler Plate

--- login-practice.php
+++ login-practice.php
@@ -1,11 +1,14 @@
+<!doctype html>
 <html>
-<title>
-Ethans Practice Login Page
-</title>
-<header>
-The Practice Login Page Yaherrr
-</header>
-
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+  <title>
+  Ethans Practice Login Page
+   </title>
+</head>
 <body>
 <h1>
 Welcome to da login page
