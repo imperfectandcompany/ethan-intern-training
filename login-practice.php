a50bced475d01e6744c422dc1cbc5db1a49c91e6
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-03-26 14:09:35 -0700

    Started The barebones minimal CSS
    Got the buttons and input set up

--- login-practice.php
+++ login-practice.php
@@ -9,15 +9,19 @@
   Ethans Practice Login Page
    </title>
 </head>
-<body>
-<h1>
-Welcome to da login page
-</h1>
-<p>
-Login With Username Password and whatever else u need
-</p>
-<h2>
-Username
-</h2>
+<body class="flex-col mx-auto pt-20 bg-red-200">
+
+<!-- allows us to define what type of method the information is going to be processed through using the method attribute ie. POST method vs GET method. Also allows us to define 
+where the information is being sent, it could be sent to the same page or another page using the attribute action-->
+<div class="flex-col bg-white text-center mx-auto border">
+<form class="justify-center">
+<div>
+<input placeholder="Username/Email" class="justify-center"></input>
+</div>
+<input placeholder="Password" class="justify-center"></input>
+<!-- allows us to deinf if this button pertains to a submission using the attribute-->
+<button class="flex bg-blue-500 mx-auto">Sign In</button>
+</form>
+</div>
 </body>
 </html>
