efdaa64c76510f09a1fd6048f749a278c17fcc9f
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-03-26 13:08:03 -0700

    Create login-practice.php

---
+++ login-practice.php
@@ -0,0 +1,20 @@
+<html>
+<title>
+Ethans Practice Login Page
+</title>
+<header>
+The Practice Login Page Yaherrr
+</header>
+
+<body>
+<h1>
+Welcome to da login page
+</h1>
+<p>
+Login With Username Password and whatever else u need
+</p>
+<h2>
+Username
+</h2>
+</body>
+</html>
