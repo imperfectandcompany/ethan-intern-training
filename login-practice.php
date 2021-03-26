99a4d229174fb3258fe793520eaa2383bb7f4d2d
Ethan Epp <77472344+EthanEpp@users.noreply.github.com>
2021-03-26 14:22:40 -0700

    Rounded Login Page
    Tweaked CSS in Login Page

--- login-practice.php
+++ login-practice.php
@@ -13,14 +13,14 @@
 
 <!-- allows us to define what type of method the information is going to be processed through using the method attribute ie. POST method vs GET method. Also allows us to define 
 where the information is being sent, it could be sent to the same page or another page using the attribute action-->
-<div class="flex-col bg-white text-center mx-auto border">
+<div class="flex-col bg-red-200 text-center mx-auto">
 <form class="justify-center">
 <div>
-<input placeholder="Username/Email" class="justify-center"></input>
+<input placeholder="Username/Email" class="justify-center m-3 p-1 rounded-md shadow-lg"></input>
 </div>
-<input placeholder="Password" class="justify-center"></input>
+<input placeholder="Password" class="justify-center m-3 p-1 rounded-md shadow-lg"></input>
 <!-- allows us to deinf if this button pertains to a submission using the attribute-->
-<button class="flex bg-blue-500 mx-auto">Sign In</button>
+<button class="flex hover:bg-blue-300 bg-blue-400 border-blue-100 outline-5 mx-auto text-grey-300 hover:text-grey-400 p-1 rounded-md shadow-md">Sign In</button>
 </form>
 </div>
 </body>
